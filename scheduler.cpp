474fe2a0da30c6a25608663972d85445dd6fd9ce
a1761407 <a1761407@student.adelaide.edu.au>
2021-08-29 19:07:10 +0930

    Update Names

--- scheduler.cpp
+++ scheduler.cpp
@@ -11,21 +11,21 @@ andrey.kan@adelaide.edu.au
 #include <fstream>
 #include <deque>
 #include <vector>
+using namespace std;
 
-// std is a namespace: https://www.cplusplus.com/doc/oldtutorial/namespaces/
 const int TIME_ALLOWANCE = 8;  // allow to use up to this number of time slots at once
 const int PRINT_LOG = 0; // print detailed execution trace
 
 class Customer
 {
 public:
-    std::string name;
+    string name;
     int priority;
     int arrival_time;
     int slots_remaining; // how many time slots are still needed
     int playing_since;
 
-    Customer(std::string par_name, int par_priority, int par_arrival_time, int par_slots_remaining)
+    Customer(string par_name, int par_priority, int par_arrival_time, int par_slots_remaining)
     {
         name = par_name;
         priority = par_priority;
@@ -49,15 +49,14 @@ public:
 };
 
 void initialize_system(
-    std::ifstream &in_file,
-    std::deque<Event> &arrival_events,
-    std::vector<Customer> &customers)
+    ifstream &in_file,
+    deque<Event> &arrival_events,
+    vector<Customer> &customers)
 {
-    std::string name;
+    string name;
     int priority, arrival_time, slots_requested;
 
     // read input file line by line
-    // https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
     int customer_id = 0;
     while (in_file >> name >> priority >> arrival_time >> slots_requested)
     {
@@ -73,58 +72,60 @@ void initialize_system(
 }
 
 void print_state(
-    std::ofstream &out_file,
+    ofstream &out_file,
     int current_time,
     int current_id,
-    const std::deque<Event> &arrival_events,
-    const std::deque<int> &customer_queue)
+    const deque<Event> &arrival_events,
+    const deque<int> &customer_queue)
 {
+    /************If (PRINT_LOG != 0), the program will not output .txt file*************/
+    
     out_file << current_time << " " << current_id << '\n';
     if (PRINT_LOG == 0)
     {
         return;
     }
-    std::cout << current_time << ", " << current_id << '\n';
+    
+    /***********************************************************************************/
+    
+    cout << current_time << ", " << current_id << '\n';
     for (int i = 0; i < arrival_events.size(); i++)
     {
-        std::cout << "\t" << arrival_events[i].event_time << ", " << arrival_events[i].customer_id << ", ";
+        cout << "\t" << arrival_events[i].event_time << ", " << arrival_events[i].customer_id << ", ";
     }
-    std::cout << '\n';
+    cout << '\n';
     for (int i = 0; i < customer_queue.size(); i++)
     {
-        std::cout << "\t" << customer_queue[i] << ", ";
+        cout << "\t" << customer_queue[i] << ", ";
     }
-    std::cout << '\n';
+    cout << '\n';
 }
 
 // process command line arguments
-// https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        std::cerr << "Provide input and output file names." << std::endl;
+        cerr << "Provide input and output file names." << endl;
         return -1;
     }
-    std::ifstream in_file(argv[1]);
-    std::ofstream out_file(argv[2]);
+    ifstream in_file(argv[1]);
+    ofstream out_file(argv[2]);
     if ((!in_file) || (!out_file))
     {
-        std::cerr << "Cannot open one of the files." << std::endl;
+        cerr << "Cannot open one of the files." << endl;
         return -1;
     }
 
-    // deque: https://www.geeksforgeeks.org/deque-cpp-stl/
-    // vector: https://www.geeksforgeeks.org/vector-in-cpp-stl/
-    std::deque<Event> arrival_events; // new customer arrivals
-    std::vector<Customer> customers; // information about each customer
+    deque<Event> arrival_events; // new customer arrivals
+    vector<Customer> customers; // information about each customer
 
     // read information from file, initialize events queue
     initialize_system(in_file, arrival_events, customers);
 
     int current_id = -1; // who is using the machine now, -1 means nobody
     int time_out = -1; // time when current customer will be preempted
-    std::deque<int> queue; // waiting queue
+    deque<int> queue; // waiting queue
 
     // step by step simulation of each time slot
     bool all_done = false;
@@ -136,6 +137,7 @@ int main(int argc, char *argv[])
             queue.push_back(arrival_events[0].customer_id);
             arrival_events.pop_front();
         }
+
         // check if we need to take a customer off the machine
         if (current_id >= 0)
         {
@@ -151,6 +153,7 @@ int main(int argc, char *argv[])
                 current_id = -1; // the machine is free now
             }
         }
+
         // if machine is empty, schedule a new customer
         if (current_id == -1)
         {
